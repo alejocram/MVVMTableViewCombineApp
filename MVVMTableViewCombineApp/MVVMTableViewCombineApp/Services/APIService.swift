94dcea69d9e444ada7f695e1667dd259e958e2c6
Alejandro Carmona Ramirez <alejocram@gmail.com>
2022-10-15 19:59:52 -0700

    Change downloadPhotos method with closure to Future of Combine

--- MVVMTableViewCombineApp/MVVMTableViewCombineApp/Services/APIService.swift
+++ MVVMTableViewCombineApp/MVVMTableViewCombineApp/Services/APIService.swift
@@ -6,20 +6,20 @@
 //
 
 import Foundation
+import Combine
 
 final class APIService {
     static let share = APIService()
     
     init() {}
     
-    func downloadPhotos(completion: @escaping (_ photos: [Photo]) -> ()) {
-        DispatchQueue.global().async {
-            sleep(3)
+    func downloadPhotos() -> Future<[Photo], Error>  {
+        return Future { promise in
             let photo1 = Photo(name: "Photo 1", url: "", description: "Balloon", date: Date(), ranking: 4.4)
             let photo2 = Photo(name: "Photo 2", url: "", description: "", date: Date(), ranking: 4.4)
             let photo3 = Photo(name: "Photo 3", url: "", description: "Dog", date: Date(), ranking: 4.4)
             let photos = [photo1, photo2, photo3]
-            completion(photos)
+            promise(.success(photos))
         }
     }
 }
--- MVVMTableViewCombineApp/MVVMTableViewCombineApp/ViewModels/PhotoListViewModel.swift
+++ MVVMTableViewCombineApp/MVVMTableViewCombineApp/ViewModels/PhotoListViewModel.swift
@@ -6,17 +6,29 @@
 //
 
 import Foundation
+import Combine
 
 final class PhotoListViewModel {
     @Published var photoNames: [String] = []
     @Published var photoDescriptions: [String?] = []
     
+    private var cancellables: [AnyCancellable] = []
+    
     func getPhotos() {
-        APIService.share.downloadPhotos { [weak self] photos in
-            for photo in photos {
-                self?.photoNames.append(photo.name)
-                self?.photoDescriptions.append(photo.description)
-            }
-        }
+        APIService.share.downloadPhotos()
+            .sink { completion in
+                switch completion {
+                case .finished:
+                    print("Publisher stopped observing")
+                case .failure(let error):
+                    print("This is any error", error)
+                }
+            } receiveValue: { [weak self] photos in
+                for photo in photos {
+                    self?.photoNames.append(photo.name)
+                    self?.photoDescriptions.append(photo.description)
+                }
+            }.store(in: &cancellables)
     }
 }
+
